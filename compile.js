920870dafba2918db2725febdf9e3aa94e934f1b
EstebanMagallonPerez <esteban1995perez@gmail.com>
2025-06-22 00:07:27 -0700

    complete refactor to enable nested templates

--- compile.js
+++ compile.js
@@ -1,68 +1,143 @@
-var fs = require('fs');
+var fs = require("fs");
 var uglify = require("uglify-js");
 
-dirname = "www/compiled"
+dirname = "www/compiled";
 
 var compiledFile = `
-function fillTemplate(template, data)
-{
-		const regex = /\{\{\\w+\}\}/gm;
-		const str = template;
-		let m;
-		while ((m = regex.exec(str)) !== null) {
-			if (m.index === regex.lastIndex) {
-				regex.lastIndex++;
-			}
-			m.forEach((match, groupIndex) => {
-				for (const [key, value] of Object.entries(data)) {
-					if(key == match.replaceAll("{","").replaceAll("}",""))
-					{
-						template = template.replaceAll(match,value)
-						break
-					}
-				}
-			});
-			m.forEach((match, groupIndex) => {
-				template = template.replaceAll(match,"")
-			});
-		}
-		return template
-}
-function getKeys(template){
-    dummyDict = {}
-    var sindex = template.indexOf("{{")
-    var eindex = template.indexOf("}}")
-    while(sindex >-1 && eindex > -1)
-    {
-        key = template.substring(sindex+2, eindex)
-        dummyDict[key] = ""
-        eindex = template.indexOf("}}",eindex+2)
-        sindex = template.indexOf("{{",sindex+2)
+function ccHandler(fileName, callback) {
+  console.log(this)
+  var hasTemplate = this.hasAttribute("data-template");
+  var isStatic = false;
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", fileName, false); // false for synchronous request
+  xmlHttp.send(null);
+  var html = xmlHttp.responseText;
+  var fileName = fileName;
+  var dataName = this.getAttribute("data-template");
+  if (hasTemplate && dataName !== undefined) {
+    try {
+      var data = JSON.parse(dataName);
+      isStatic = true;
+    } catch {
+      var templateData = getKeys(html);
+      try {
+        var data = eval(dataName);
+        if (Array.isArray(data) == false) {
+          data = Object.assign(templateData, data);
+        }
+      } catch {
+        eval("window." + dataName + "= templateData");
+        var data = eval(dataName);
+      }
     }
-    return dummyDict
+  }
+  if (hasTemplate) {
+    if (Array.isArray(data)) {
+      for (var i = 0; i < data.length; i++) {
+        var sibling = document.createElement(this.tagName);
+        sibling.setAttribute(
+          "data-template",
+          dataName + "[" + (data.length - i - 1) + "]"
+        );
+        this.insertAdjacentElement("afterend", sibling);
+      }
+      this.remove();
+      return;
+    }
+    if (isStatic === false) {
+      eval(dataName + " = createTracker(this," + dataName + ")");
+    }
+    html = fillTemplate(html, data);
+    this.updateTemplate = function (data) {
+      var html = fillTemplate(xmlHttp.responseText, data);
+      res = htmlToElement(html);
+      this.innerHTML = res.outerHTML;
+    };
+  }
+  let res = htmlToElement(html);
+  let slot = res.getElementsByTagName("slot");
+  if (slot.length > 0) {
+    let p = slot[0].parentNode;
+    slot[0].remove();
+    var div_array = [...this.children]; // converts NodeList to Array
+    div_array.forEach((div) => {
+      p.append(div);
+    });
+  }
+  this.append(res);
+  callback.bind(this)()
+
+
+}
+function fillTemplate(template, data) {
+	const regex = /\{\{\\w+\}\}/gm;
+  const str = template;
+  let m;
+  while ((m = regex.exec(str)) !== null) {
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+    m.forEach((match, groupIndex) => {
+      for (const [key, value] of Object.entries(data)) {
+        if (key == match.replaceAll("{", "").replaceAll("}", "")) {
+          if (typeof value !== typeof "") {
+            let value1 = JSON.stringify(value);
+            template = template.replaceAll(match, value1);
+          } else {
+            template = template.replaceAll(match, value);
+          }
+          break;
+        }
+      }
+    });
+    m.forEach((match, groupIndex) => {
+      template = template.replaceAll(match, "");
+    });
+  }
+  return template;
+}
+function getKeys(template) {
+  dummyDict = {};
+  var sindex = template.indexOf("{{");
+  var eindex = template.indexOf("}}");
+  while (sindex > -1 && eindex > -1) {
+    key = template.substring(sindex + 2, eindex);
+    dummyDict[key] = "";
+    eindex = template.indexOf("}}", eindex + 2);
+    sindex = template.indexOf("{{", sindex + 2);
+  }
+  return dummyDict;
 }
 function htmlToElement(html) {
-    var template = document.createElement('template');
-    html = html.trim(); // Never return a text node of whitespace as the result
-    data = {}
-    html = fillTemplate(html,data)
-    template.innerHTML = html;
-    return template.content.firstChild;
+  var template = document.createElement("template");
+  html = html.trim(); // Never return a text node of whitespace as the result
+  data = {};
+  html = fillTemplate(html, data);
+  template.innerHTML = html;
+  return template.content.firstChild;
 }
-function createTracker(context,data){
-    dataWithWatcher = \`var x = {listener: undefined,
+
+
+function createTracker(context, data) {
+  if (data.listener != undefined) {
+    return data;
+  }
+  dataWithWatcher = \`var x = {listener: undefined,
             set (){
                 this.internal = val;
                 this.listener(val);
             },
             registerListener: function(listener) {
             this.listener = listener;
-        },\`
-    var length = Object.keys(data).length
-    var count = 0
-    for (var [key, value] of Object.entries(data)) {
+        },\`;
+  var length = Object.keys(data).length;
+  var count = 0;
+  for (var [key, value] of Object.entries(data)) {
         if (typeof value === "string") {
             value = "\\"" + value + "\\""
+        }
+        if (typeof value === "object") {
+            value = JSON.stringify(value)
         }
         dataWithWatcher += key + \`Internal : \` + value + \`,
             set \`+ key + \`(val) {
@@ -74,75 +149,31 @@ function createTracker(context,data){
             }\`
         if (count < length) { dataWithWatcher += "," }
         count++;
-    }
-    dataWithWatcher += "}"
-    eval(dataWithWatcher)
-    x.registerListener(function (val) {
-        context.updateTemplate(this)
-    })
-    return x;
-}`
+    } 
+  dataWithWatcher += "}";
+
+  eval(dataWithWatcher);
+  x.registerListener(function (val) {
+    context.updateTemplate(this);
+  });
+  return x;
+}`;
 var component = `
 class CLASSNAME_REPLACE extends HTMLElement {
     constructor() {
         super();
         // element created
     }
-    connectedCallback() {
-        var hasTemplate = this.hasAttribute("data-template")
-        var isStatic = false
-        var xmlHttp = new XMLHttpRequest();
-		xmlHttp.open( "GET", "FILENAME_REPLACE", false ); // false for synchronous request
-        xmlHttp.send(null);
-        
-        var html = xmlHttp.responseText
-        if (hasTemplate) {
-            var dataName = this.getAttribute("data-template");
-            try{
-                var data = JSON.parse(dataName)
-                isStatic = true
-            }catch{
-                var templateData = getKeys(html)
-                try{
-                    var data = eval(dataName)
-                    if(Array.isArray(data) == false)
-                    {
-                        data = Object.assign(templateData, data)
-                    }
-                }catch{
-                    eval("window."+dataName +"= templateData")
-                    var data = eval(dataName)
-                }
-                
-            }
-        }
-        if (hasTemplate) {
-            if (Array.isArray(data)) {
-                for (var i = 0; i < data.length; i++) {
-                    var sibling = document.createElement(this.tagName)
-                    sibling.setAttribute("data-template",dataName+"["+(data.length-i-1)+"]")
-                    this.insertAdjacentElement("afterend", sibling)
-                }
-                this.remove()
-                return;
-            }
-            if(isStatic === false){
-                eval(dataName+" = createTracker(this,"+dataName+")")
+  customOnload(){
+    CUSTOM_ONLOAD
+  }
 
-            }
-            html = fillTemplate(html, data)
-            this.updateTemplate = function(data){
-            var html = fillTemplate(xmlHttp.responseText,data)
-            res = htmlToElement(html)
-            this.innerHTML = res.outerHTML
-            //this.shadowRoot.innerHTML = html
-            }
-        }
-        var res = htmlToElement(html)
-        //this.shadowRoot.append(res);
-        this.append(res);
-		CUSTOM_ONLOAD
-    }
+  connectedCallback() {
+    document.addEventListener("DOMContentLoaded", function () {
+      ccHandler.bind(this,"FILENAME_REPLACE",this.customOnload)()
+      
+    }.bind(this,self));
+  }
 
     disconnectedCallback() {
         // browser calls this method when the element is removed from the document
@@ -163,59 +194,66 @@ class CLASSNAME_REPLACE extends HTMLElement {
     }
 
     // there can be other element methods and properties
-}\r`
+}\r`;
 function onError(err) {
-	console.log(err)
+  console.log(err);
 }
-templateFolder = "www/template"
-var globalStyle = ""
+templateFolder = "www/template";
+var globalStyle = "";
 fs.readdir(templateFolder, function (err, filenames) {
+  if (err !== null) {
+    onError(err);
+    return;
+  }
+  var count = 0;
+  filenames.forEach(function (filename) {
+    console.log("compiling ", filename);
+    let componentText = component
+      .replace("FILENAME_REPLACE", "/template/" + filename)
+      .replace("FILENAME_REPLACE", "/template/" + filename)
+      .replace("CLASSNAME_REPLACE", "template_" + count);
+    componentText +=
+      'customElements.define("' +
+      filename.replace(".html", "") +
+      '", template_' +
+      count +
+      ");";
+    var content = fs.readFileSync(templateFolder + "/" + filename, "utf8");
+    var scriptContent = "";
+    if (content.indexOf("<script>") > -1) {
+      scriptContent = content.split("<script>")[1].split("</script>")[0];
+    }
+    var styleContent = "";
+    if (content.indexOf("<style>") > -1) {
+      globalStyle += content.split("<style>")[1].split("</style>")[0];
+    }
+    componentText = componentText.replace("CUSTOM_ONLOAD", scriptContent);
+    compiledFile += componentText;
+    console.log("linting ", filename);
+    console.log("compiled ", filename);
+    count++;
+  });
+  console.log("spitting out the compiled JS", "compiled.js");
+  fs.writeFile(dirname + "/compiled.js", compiledFile, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+  var uglified = uglify.minify([compiledFile]);
 
-	if (err !== null) {
-		onError(err);
-		return;
-	}
-	var count = 0;
-	filenames.forEach(function (filename) {
-		console.log("compiling ",filename)
-		let componentText = component.replace("FILENAME_REPLACE", "/template/" + filename).replace("CLASSNAME_REPLACE", "template_" + count);
-		componentText += 'customElements.define("' + filename.replace(".html", "") + '", template_' + count + ');';
-		var content = fs.readFileSync(templateFolder+"/" + filename, 'utf8');
-		var scriptContent = ""
-		if (content.indexOf("<script>") > -1) {
-			scriptContent = content.split("<script>")[1].split("</script>")[0]
-		}
-        var styleContent = ""
-        if (content.indexOf("<style>") > -1) {
-			globalStyle += content.split("<style>")[1].split("</style>")[0]
-		}
-		componentText = componentText.replace("CUSTOM_ONLOAD", scriptContent)
-		compiledFile += componentText;
-		console.log("linting ",filename)
-		console.log("compiled ",filename)
-		count++;
-	});
-	console.log("spitting out the compiled JS", 'compiled.js')
-	fs.writeFile(dirname + '/compiled.js', compiledFile, err => {
-		if (err) {
-			console.error(err);
-		}
-	});
-	var uglified = uglify.minify([compiledFile]);
-
-	fs.writeFile(dirname + '/compiled.min.js', uglified.code, function (err) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log("Barfing out the minified JS ", 'compiled.min.js');
-		}
-	});
-    fs.writeFile(dirname + '/style.css', globalStyle, function (err) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log("Spitting out the stylesheet", 'style.css');
-		}
-	});
+  fs.writeFile(dirname + "/compiled.min.js", uglified.code, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Barfing out the minified JS ", "compiled.min.js");
+    }
+  });
+  fs.writeFile(dirname + "/style.css", globalStyle, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Spitting out the stylesheet", "style.css");
+    }
+  });
 });
-var customComponent = ''
+var customComponent = "";
--- www/compiled/compiled.js
+++ www/compiled/compiled.js
@@ -1,63 +1,138 @@
 
-function fillTemplate(template, data)
-{
-		const regex = /{{\w+}}/gm;
-		const str = template;
-		let m;
-		while ((m = regex.exec(str)) !== null) {
-			if (m.index === regex.lastIndex) {
-				regex.lastIndex++;
-			}
-			m.forEach((match, groupIndex) => {
-				for (const [key, value] of Object.entries(data)) {
-					if(key == match.replaceAll("{","").replaceAll("}",""))
-					{
-						template = template.replaceAll(match,value)
-						break
-					}
-				}
-			});
-			m.forEach((match, groupIndex) => {
-				template = template.replaceAll(match,"")
-			});
-		}
-		return template
-}
-function getKeys(template){
-    dummyDict = {}
-    var sindex = template.indexOf("{{")
-    var eindex = template.indexOf("}}")
-    while(sindex >-1 && eindex > -1)
-    {
-        key = template.substring(sindex+2, eindex)
-        dummyDict[key] = ""
-        eindex = template.indexOf("}}",eindex+2)
-        sindex = template.indexOf("{{",sindex+2)
+function ccHandler(fileName, callback) {
+  console.log(this)
+  var hasTemplate = this.hasAttribute("data-template");
+  var isStatic = false;
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", fileName, false); // false for synchronous request
+  xmlHttp.send(null);
+  var html = xmlHttp.responseText;
+  var fileName = fileName;
+  var dataName = this.getAttribute("data-template");
+  if (hasTemplate && dataName !== undefined) {
+    try {
+      var data = JSON.parse(dataName);
+      isStatic = true;
+    } catch {
+      var templateData = getKeys(html);
+      try {
+        var data = eval(dataName);
+        if (Array.isArray(data) == false) {
+          data = Object.assign(templateData, data);
+        }
+      } catch {
+        eval("window." + dataName + "= templateData");
+        var data = eval(dataName);
+      }
     }
-    return dummyDict
+  }
+  if (hasTemplate) {
+    if (Array.isArray(data)) {
+      for (var i = 0; i < data.length; i++) {
+        var sibling = document.createElement(this.tagName);
+        sibling.setAttribute(
+          "data-template",
+          dataName + "[" + (data.length - i - 1) + "]"
+        );
+        this.insertAdjacentElement("afterend", sibling);
+      }
+      this.remove();
+      return;
+    }
+    if (isStatic === false) {
+      eval(dataName + " = createTracker(this," + dataName + ")");
+    }
+    html = fillTemplate(html, data);
+    this.updateTemplate = function (data) {
+      var html = fillTemplate(xmlHttp.responseText, data);
+      res = htmlToElement(html);
+      this.innerHTML = res.outerHTML;
+    };
+  }
+  let res = htmlToElement(html);
+  let slot = res.getElementsByTagName("slot");
+  if (slot.length > 0) {
+    let p = slot[0].parentNode;
+    slot[0].remove();
+    var div_array = [...this.children]; // converts NodeList to Array
+    div_array.forEach((div) => {
+      p.append(div);
+    });
+  }
+  this.append(res);
+  callback.bind(this)()
+
+
+}
+function fillTemplate(template, data) {
+	const regex = /{{\w+}}/gm;
+  const str = template;
+  let m;
+  while ((m = regex.exec(str)) !== null) {
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+    m.forEach((match, groupIndex) => {
+      for (const [key, value] of Object.entries(data)) {
+        if (key == match.replaceAll("{", "").replaceAll("}", "")) {
+          if (typeof value !== typeof "") {
+            let value1 = JSON.stringify(value);
+            template = template.replaceAll(match, value1);
+          } else {
+            template = template.replaceAll(match, value);
+          }
+          break;
+        }
+      }
+    });
+    m.forEach((match, groupIndex) => {
+      template = template.replaceAll(match, "");
+    });
+  }
+  return template;
+}
+function getKeys(template) {
+  dummyDict = {};
+  var sindex = template.indexOf("{{");
+  var eindex = template.indexOf("}}");
+  while (sindex > -1 && eindex > -1) {
+    key = template.substring(sindex + 2, eindex);
+    dummyDict[key] = "";
+    eindex = template.indexOf("}}", eindex + 2);
+    sindex = template.indexOf("{{", sindex + 2);
+  }
+  return dummyDict;
 }
 function htmlToElement(html) {
-    var template = document.createElement('template');
-    html = html.trim(); // Never return a text node of whitespace as the result
-    data = {}
-    html = fillTemplate(html,data)
-    template.innerHTML = html;
-    return template.content.firstChild;
+  var template = document.createElement("template");
+  html = html.trim(); // Never return a text node of whitespace as the result
+  data = {};
+  html = fillTemplate(html, data);
+  template.innerHTML = html;
+  return template.content.firstChild;
 }
-function createTracker(context,data){
-    dataWithWatcher = `var x = {listener: undefined,
+
+
+function createTracker(context, data) {
+  if (data.listener != undefined) {
+    return data;
+  }
+  dataWithWatcher = `var x = {listener: undefined,
             set (){
                 this.internal = val;
                 this.listener(val);
             },
             registerListener: function(listener) {
             this.listener = listener;
-        },`
-    var length = Object.keys(data).length
-    var count = 0
-    for (var [key, value] of Object.entries(data)) {
+        },`;
+  var length = Object.keys(data).length;
+  var count = 0;
+  for (var [key, value] of Object.entries(data)) {
         if (typeof value === "string") {
             value = "\"" + value + "\""
+        }
+        if (typeof value === "object") {
+            value = JSON.stringify(value)
         }
         dataWithWatcher += key + `Internal : ` + value + `,
             set `+ key + `(val) {
@@ -69,74 +144,30 @@ function createTracker(context,data){
             }`
         if (count < length) { dataWithWatcher += "," }
         count++;
-    }
-    dataWithWatcher += "}"
-    eval(dataWithWatcher)
-    x.registerListener(function (val) {
-        context.updateTemplate(this)
-    })
-    return x;
+    } 
+  dataWithWatcher += "}";
+
+  eval(dataWithWatcher);
+  x.registerListener(function (val) {
+    context.updateTemplate(this);
+  });
+  return x;
 }
 class template_0 extends HTMLElement {
     constructor() {
         super();
         // element created
     }
-    connectedCallback() {
-        var hasTemplate = this.hasAttribute("data-template")
-        var isStatic = false
-        var xmlHttp = new XMLHttpRequest();
-		xmlHttp.open( "GET", "/template/react-banner.html", false ); // false for synchronous request
-        xmlHttp.send();
-        
-        var html = xmlHttp.responseText
-        if (hasTemplate) {
-            var dataName = this.getAttribute("data-template");
-            try{
-                var data = JSON.parse(dataName)
-                isStatic = true
-            }catch{
-                var templateData = getKeys(html)
-                try{
-                    var data = eval(dataName)
-                    if(Array.isArray(data) == false)
-                    {
-                        data = Object.assign(templateData, data)
-                    }
-                }catch{
-                    eval("window."+dataName +"= templateData")
-                    var data = eval(dataName)
-                }
-                
-            }
-        }
-        if (hasTemplate) {
-            if (Array.isArray(data)) {
-                for (var i = 0; i < data.length; i++) {
-                    var sibling = document.createElement(this.tagName)
-                    sibling.setAttribute("data-template",dataName+"["+(data.length-i-1)+"]")
-                    this.insertAdjacentElement("afterend", sibling)
-                }
-                this.remove()
-                return;
-            }
-            if(isStatic === false){
-                eval(dataName+" = createTracker(this,"+dataName+")")
+  customOnload(){
+    
+  }
 
-            }
-            html = fillTemplate(html, data)
-            this.updateTemplate = function(data){
-            var html = fillTemplate(xmlHttp.responseText,data)
-            res = htmlToElement(html)
-            this.innerHTML = res.outerHTML
-            //this.shadowRoot.innerHTML = html
-            }
-        }
-        var res = htmlToElement(html)
-        //this.shadowRoot.append(res);
-        this.append(res);
-		
-    }
+  connectedCallback() {
+    document.addEventListener("DOMContentLoaded", function () {
+      ccHandler.bind(this,"/template/carousel-caption.html",this.customOnload)()
+      
+    }.bind(this,self));
+  }
 
     disconnectedCallback() {
         // browser calls this method when the element is removed from the document
@@ -157,70 +188,36 @@ class template_0 extends HTMLElement {
     }
 
     // there can be other element methods and properties
-}
-customElements.define("react-banner", template_0);
+}customElements.define("carousel-caption", template_0);
 class template_1 extends HTMLElement {
     constructor() {
         super();
         // element created
     }
-    connectedCallback() {
-        var hasTemplate = this.hasAttribute("data-template")
-        var isStatic = false
-        var xmlHttp = new XMLHttpRequest();
-		xmlHttp.open( "GET", "/template/react-card.html", false ); // false for synchronous request
-        xmlHttp.send();
-        
-        var html = xmlHttp.responseText
-        if (hasTemplate) {
-            var dataName = this.getAttribute("data-template");
-            try{
-                var data = JSON.parse(dataName)
-                isStatic = true
-            }catch{
-                var templateData = getKeys(html)
-                try{
-                    var data = eval(dataName)
-                    if(Array.isArray(data) == false)
-                    {
-                        data = Object.assign(templateData, data)
-                    }
-                }catch{
-                    eval("window."+dataName +"= templateData")
-                    var data = eval(dataName)
-                }
-                
-            }
-        }
-        if (hasTemplate) {
-            if (Array.isArray(data)) {
-                for (var i = 0; i < data.length; i++) {
-                    var sibling = document.createElement(this.tagName)
-                    sibling.setAttribute("data-template",dataName+"["+(data.length-i-1)+"]")
-                    this.insertAdjacentElement("afterend", sibling)
-                }
-                this.remove()
-                return;
-            }
-            if(isStatic === false){
-                eval(dataName+" = createTracker(this,"+dataName+")")
+  customOnload(){
+    
+  let id = this.children[0].getAttribute("id")
+  let carouselItems = document.querySelectorAll("carousel-item");
+  let cArray = [...carouselItems]
+  let output = ""
+  let count = 0;
+  cArray.forEach(element => {
+    output += '<button type="button" data-bs-target="'
+      + id + '"' + (count == 0 ? 'class="active"' : "")
+      + ' data-bs-slide-to="' + count + '" aria-current="true" aria-label="Slide ' + (count + 1) + '"></button>'
+    count += 1
+  });
+  let one = document.querySelectorAll(".carousel-indicators");
+  one[0].innerHTML = output;
 
-            }
-            html = fillTemplate(html, data)
-            this.updateTemplate = function(data){
-            var html = fillTemplate(xmlHttp.responseText,data)
-            res = htmlToElement(html)
-            this.innerHTML = res.outerHTML
-            //this.shadowRoot.innerHTML = html
-            }
-        }
-        var res = htmlToElement(html)
-        //this.shadowRoot.append(res);
-        this.append(res);
-		
-    console.log("Script loading works")
+  }
 
-    }
+  connectedCallback() {
+    document.addEventListener("DOMContentLoaded", function () {
+      ccHandler.bind(this,"/template/carousel-container.html",this.customOnload)()
+      
+    }.bind(this,self));
+  }
 
     disconnectedCallback() {
         // browser calls this method when the element is removed from the document
@@ -241,5 +238,40 @@ class template_1 extends HTMLElement {
     }
 
     // there can be other element methods and properties
-}
-customElements.define("react-card", template_1);
+}customElements.define("carousel-container", template_1);
+class template_2 extends HTMLElement {
+    constructor() {
+        super();
+        // element created
+    }
+  customOnload(){
+    
+  }
+
+  connectedCallback() {
+    document.addEventListener("DOMContentLoaded", function () {
+      ccHandler.bind(this,"/template/carousel-item.html",this.customOnload)()
+      
+    }.bind(this,self));
+  }
+
+    disconnectedCallback() {
+        // browser calls this method when the element is removed from the document
+        // (can be called many times if an element is repeatedly added/removed)
+    }
+
+    static get observedAttributes() {
+        return [/* array of attribute names to monitor for changes */];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        // called when one of attributes listed above is modified
+    }
+
+    adoptedCallback() {
+        // called when the element is moved to a new document
+        // (happens in document.adoptNode, very rarely used)
+    }
+
+    // there can be other element methods and properties
+}customElements.define("carousel-item", template_2);
